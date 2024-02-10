4d7480a5b31e09ac7367d502266d0d68a32bbef4
Brennan Davenport <brennandavenport9@gmail.com>
2024-02-10 14:04:43 -0500

    food data

--- src/food.py
+++ src/food.py
@@ -6,6 +6,8 @@ class FoodItem:
         self.fat = fat
         self.protein = protein
 
+# Carbs, Fat, and Protein are in grams
+
 # Grilled Angus Beef Burger
 grilled_angus_beef_burger = FoodItem("Grilled Angus Beef Burger", 280, 44, 
 26, 33)
@@ -28,6 +30,27 @@ turkey_burger = FoodItem("Turkey Burger", 291, 28, 18, 17)
 # Normal Buns
 normal_buns = FoodItem("Normal Buns", 203, 38, 3, 6)
 
+# Bacon Toppings
+bacon_topping = FoodItem("Bacon Topping", 128, 0.7, 11.3, 7.1)
+
+# Red Onion Toppings, Sliced
+onion_red_topping_sliced = FoodItem("Red Onion Topping, Sliced", 12, 2.8, 0, 0.3)
+
+# Fresh Tomato Topping, Sliced
+fresh_tomato_topping_sliced = FoodItem("Tomato Topping, Sliced", 5, 1.1, 0.1, 0.2)
+
+# Green Leaf Lettuce Topping
+green_leaf_lettuce_topping = FoodItem("Green Leaf Lettuce Topping", 2, 0.4, 0, 0.2)
+
+# Dill Pickle, Sliced Topping
+dill_pickle_topping_sliced = FoodItem("Dill Pickle Topping, Sliced", 0, 1, 0, 0)
+
+#Sautéed Onions Topping
+sauteed_onion_topping = FoodItem("Sautéed Onions Topping", 22, 4, 0.6, 0.5)
+
+#Dill Pickle, Spears Topping
+dill_pickle_topping_spear = FoodItem("Dill Pickle Topping, Spear", 0, 1.30, 0, 0)
+
 # Grilled Chicken with Normal Buns
 grilled_chicken_with_buns = FoodItem(
     "Grilled Chicken with Normal Buns",
@@ -45,4 +68,31 @@ grilled_angus_beef_burger_with_buns = FoodItem(
     grilled_angus_beef_burger.fat + normal_buns.fat,
     grilled_angus_beef_burger.protein + normal_buns.protein
 )
+
+#Fresh Carrots, Shredded
+fresh_carrots_shredded = FoodItem("Fresh Carrots, Shredded",12,3,0,0)
+
+#Fresh Cilantro
+fresh_cilantro = FoodItem("Fresh Cilantro",0,0,0,0)
+
+#Fresh Green Onions
+fresh_green_onions = FoodItem("Fresh Green Onions",5,1,0,0)
+
+#Fresh Jalapeno, Sliced
+fresh_jalapeno_sliced = FoodItem("Fresh Jalapeno, Sliced",4.1,0,0,0)
+
+#Fresh Lime Wedge
+fresh_lime_wedge = FoodItem("Fresh Lime Wedge", 3,1,0,0)
+
+#Fresh Yellow Onions, Sliced
+fresh_yellow_onions_sliced = FoodItem("Fresh Yellow Onions, Sliced",5,1,0,0)
+
+#Pho with Chicken, Vegetables & Noodles
+pho_with_chicken_vegetables_and_noodles = FoodItem("",361,62,3.70,19)
+
+#Tofu
+tofu = FoodItem("Tofu",80,0,8,2)
+
+#Vegetarian Pho with Tofu, Vegetables & Noodles
+vegetarian_pho_with_tofu_vegetables_and_noodles = FoodItem("Vegetarian Pho with Tofu, Vegetables & Noodles", 501,66,22,10)
 
