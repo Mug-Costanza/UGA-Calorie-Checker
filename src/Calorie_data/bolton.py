32d9a2ba6763f554f2de7f697f35ec8447ab2311
Brennan Davenport <brennandavenport@Brennans-MacBook-Pro.local>
2024-02-10 10:36:17 -0500

    Burger Data

---
+++ src/Calorie_data/bolton.py
@@ -0,0 +1,55 @@
+#Data used to calculate nutrition facts
+
+#Grilled Angus Beef Burger
+grilled_angus_beef_patty_calories = 280
+grilled_angus_beef_patty_carbs = 44
+grilled_angus_beef_patty_fat = 26
+grilled_angus_beef_patty_protein = 33
+
+#Grilled Chicken
+grilled_chicken_calories = 140
+grilled_chicken_carbs = 30
+grilled_chicken_fat = 20
+grilled_chicken_protein = 46
+
+#Grilled Cheese
+grilled_cheese_calories = 320
+grilled_cheese_carbs = 213
+grilled_cheese_fat = 15
+grilled_cheese_protein = 15 
+
+#Nathan's Hotdogs
+nathans_hotdogs_calories = 182
+nathans_hotdogs_carbs = 20 
+nathans_hotdogs_fat = 13 
+nathans_hotdogs_protein = 6
+
+#French Fries
+french_fries_calories = 260
+french_fries_carbs = 29  
+french_fries_fat = 13  
+french_fries_protein = 3  
+
+#Turkey Burger
+turkey_burger_calories = 291
+turkey_burger_carbs = 28
+turkey_burger_fat = 18
+turkey_burger_protein = 17
+
+#Normal Buns
+normal_buns_calories = 203
+normal_buns_carbs = 38
+normal_buns_fat = 3
+normal_buns_protein = 6
+
+#Grilled Chicken with Normal Buns
+grilled_chicken_with_buns_calories = grilled_chicken_calories + normal_buns_calories
+grilled_chicken_with_buns_carbs = grilled_chicken_carbs + normal_buns_carbs
+grilled_chicken_with_buns_fat = grilled_chicken_fat + normal_buns_fat
+grilled_chicken_with_buns_protein = grilled_chicken_protein + normal_buns_protein
+
+#Grilled Angus Beef Burger with Normal Buns
+grilled_angus_beef_burger_with_buns_calories = grilled_angus_beef_patty_calories + normal_buns_calories
+grilled_angus_beef_burger_with_buns_carbs = grilled_angus_beef_patty_carbs + normal_buns_carbs
+grilled_angus_beef_burger_with_buns_fat = grilled_angus_beef_patty_fat + normal_buns_fat
+grilled_angus_beef_burger_with_buns_protein = grilled_angus_beef_patty_protein + normal_buns_protein
